1887f95279acc9b71a6d6d24fc83bc67dd3b76c8
Luke Marsden <luke@clusterhq.com>
2015-01-08 15:35:41 +0000

    run.sh from what I can remember of it

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo starting good...
+
+tcpdump -i lo -w outputs/good-${1}.pcap
+pid=$!
+sleep 1
+
+DOCKER_HOST=localhost:4243 docker run ubuntu echo hello
+
+sleep 1
+kill $pid
+
+echo done
+
+echo starting bad...
+
+tcpdump -i lo -w outputs/bad-${1}.pcap
+pid=$!
+sleep 1
+
+DOCKER_HOST=localhost:2375 docker run ubuntu echo hello
+
+sleep 1
+kill $pid
+
+echo done
+
