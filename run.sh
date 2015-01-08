4ca6ced644a81713c693db6420fda9ebbf11cc7e
Luke Marsden <luke@clusterhq.com>
2015-01-08 15:38:18 +0000

    get the ports the right way round

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ tcpdump -i lo -w outputs/good-${1}.pcap
 pid=$!
 sleep 1
 
-DOCKER_HOST=localhost:4243 docker run ubuntu echo hello
+DOCKER_HOST=localhost:2375 docker run ubuntu echo hello
 
 sleep 1
 kill $pid
@@ -19,7 +19,7 @@ tcpdump -i lo -w outputs/bad-${1}.pcap
 pid=$!
 sleep 1
 
-DOCKER_HOST=localhost:2375 docker run ubuntu echo hello
+DOCKER_HOST=localhost:4243 docker run ubuntu echo hello
 
 sleep 1
 kill $pid
