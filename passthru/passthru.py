3065ac265cff587d05f7263bccf323f035005664
Luke Marsden <luke@clusterhq.com>
2015-01-08 08:52:00 -0800

    comment out stuff, and also enable switching out of http into raw tcp proxying mode

--- passthru/passthru.py
+++ passthru/passthru.py
@@ -10,75 +10,75 @@ _debug = []
 
 USE_HTTP_PROXY = True
 
-@implementer(IHalfCloseableProtocol)
-class ServerProtocol(protocol.Protocol):
-    def __init__(self, dockerAddr, dockerPort):
-        self.buffer = ""
-        self.client = None
-        self.dockerAddr = dockerAddr
-        self.dockerPort = dockerPort
+# @implementer(IHalfCloseableProtocol)
+# class ServerProtocol(protocol.Protocol):
+#     def __init__(self, dockerAddr, dockerPort):
+#         self.buffer = ""
+#         self.client = None
+#         self.dockerAddr = dockerAddr
+#         self.dockerPort = dockerPort
 
-    def connectionMade(self):
-        factory = protocol.ClientFactory()
-        factory.protocol = ClientProtocol
-        factory.server = self
+#     def connectionMade(self):
+#         factory = protocol.ClientFactory()
+#         factory.protocol = ClientProtocol
+#         factory.server = self
 
-        reactor.connectTCP(
-            self.dockerAddr, self.dockerPort, factory)
+#         reactor.connectTCP(
+#             self.dockerAddr, self.dockerPort, factory)
 
-    # Client => Proxy
-    def dataReceived(self, data):
-        if self.client:
-            self.client.write(data)
-        else:
-            self.buffer += data
+#     # Client => Proxy
+#     def dataReceived(self, data):
+#         if self.client:
+#             self.client.write(data)
+#         else:
+#             self.buffer += data
 
-    # Proxy => Client
-    def write(self, data):
-        self.transport.write(data)
+#     # Proxy => Client
+#     def write(self, data):
+#         self.transport.write(data)
 
-    def readConnectionLost(self):
-        self.client.transport.loseWriteConnection()
+#     def readConnectionLost(self):
+#         self.client.transport.loseWriteConnection()
 
-    def connectionLost(self, reason):
-        self.client.transport.loseConnection()
-
-
-class ClientProtocol(protocol.Protocol):
-
-    def connectionMade(self):
-        self.factory.server.client = self
-        if self.factory.server.buffer:
-            self.write(self.factory.server.buffer)
-            self.factory.server.buffer = ''
-
-    # Server => Proxy
-    def dataReceived(self, data):
-        self.factory.server.write(data)
-
-    # Proxy => Server
-    def write(self, data):
-        if data:
-            self.transport.write(data)
+#     def connectionLost(self, reason):
+#         self.client.transport.loseConnection()
 
 
-    def connectionLost(self, reason):
-        self.factory.server.transport.loseConnection()
+# class ClientProtocol(protocol.Protocol):
+
+#     def connectionMade(self):
+#         self.factory.server.client = self
+#         if self.factory.server.buffer:
+#             self.write(self.factory.server.buffer)
+#             self.factory.server.buffer = ''
+
+#     # Server => Proxy
+#     def dataReceived(self, data):
+#         self.factory.server.write(data)
+
+#     # Proxy => Server
+#     def write(self, data):
+#         if data:
+#             self.transport.write(data)
+
+
+#     def connectionLost(self, reason):
+#         self.factory.server.transport.loseConnection()
 
 
 
-class ServerProtocolFactory(protocol.ServerFactory):
+# class ServerProtocolFactory(protocol.ServerFactory):
 
-    protocol = ServerProtocol
+#     protocol = ServerProtocol
 
-    def __init__(self, dockerAddr, dockerPort):
-        self.dockerAddr = dockerAddr
-        self.dockerPort = dockerPort
+#     def __init__(self, dockerAddr, dockerPort):
+#         self.dockerAddr = dockerAddr
+#         self.dockerPort = dockerPort
 
-    def buildProtocol(self, addr):
-        p = self.protocol(self.dockerAddr, self.dockerPort)
-        p.factory = self
-        return p
+#     def buildProtocol(self, addr):
+#         p = self.protocol(self.dockerAddr, self.dockerPort)
+#         p.factory = self
+#         return p
 
 
 
@@ -121,6 +121,8 @@ class DockerProxyClient(proxy.ProxyClient):
     def handleHeader(self, key, value):
         if self.log:
             channelLog("dockerapi/%d" % (id(self),), "handling header", key, value)
+        if key.lower() == "content-type" and value == "application/vnd.docker.raw-stream":
+            self.setRawMode()
         return proxy.ProxyClient.handleHeader(self, key, value)
 
 
@@ -140,7 +142,16 @@ class DockerProxyClient(proxy.ProxyClient):
         if self.log:
             channelLog("dockerapi/%d" % (id(self),),
                     "line_mode:", self.line_mode, "data:", repr(data))
-        return basic.LineReceiver.dataReceived(self, data)
+        return proxy.ProxyClient.dataReceived(self, data)
+
+    framed = 0
+    def rawDataReceived(self, data):
+        if self.framed < 2:
+            framing = data[:2 - self.framed]
+            data = data[2 - self.framed:]
+            self.framed += len(framing)
+        if data:
+            self.father.channel.transport.write(data)
 
 
 class DockerProxyClientFactory(proxy.ProxyClientFactory):
