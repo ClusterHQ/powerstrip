93a819394ec8ed050d0cdecd9b737fae06c70f02
Luke Marsden <luke@clusterhq.com>
2015-01-11 17:54:23 +0000

    pep8 and treq

--- passthru/test/test_core.py
+++ passthru/test/test_core.py
@@ -23,70 +23,70 @@ class ProxyTests(TestCase):
         self.dockerPort = self.server.getHost().port
 
 
-    def _getProxyInstance(self, configuration):
+    def _get_proxy_instance(self, configuration):
         """
         Given a yaml configuration (with placeholders for interpolation of
         current runtime ports for the test), return an appropriately configured
         proxy instance.
         """
 
-    def test_emptyEndpoints(self):
+    def test_empty_endpoints(self):
         """
         The proxy passes through requests when no endpoints are specified.
         """
 
-    def test_endpointAndEmptyHooks(self):
+    def test_endpoint_and_empty_hooks(self):
         """
         An endpoint is specified, but no pre-or post hooks are added to it.
         Requests to the endpoint are proxied.
         """
 
-    def test_addingPreHookPlugin(self):
+    def test_adding_pre_hook_plugin(self):
         """
         A plugin has a pre-hook which increments an integral field in the JSON
         POST body called "Number".
         """
 
-    def test_addingPreHookTwicePlugin(self):
+    def test_adding_pre_hook_twice_plugin(self):
         """
         Chaining pre-hooks: adding twice means you get +2.
         """
 
-    def test_addingPostHookPlugin(self):
+    def test_adding_post_hook_plugin(self):
         """
         A plugin has a post-hook which increments an integral field in the JSON
         response body called "Number".
         """
     
-    def test_addingPostHookTwicePlugin(self):
+    def test_adding_post_hook_twice_plugin(self):
         """
         Chaining post-hooks: adding twice means you get +2.
         """
 
-    def test_prehookErrorDoesNotCallDocker(self):
+    def test_prehook_error_does_not_call_docker(self):
         """
         An error in the pre-hook does not call through to Docker and returns
         the error to the user.
         """
 
-    def test_prehookErrorStopsChain(self):
+    def test_prehook_error_stops_chain(self):
         """
         An error in the pre-hook stops the chain when there are multiple
         pre-hooks.
         """
 
-    def test_posthookErrorStopsChain(self):
+    def test_posthook_error_stops_chain(self):
         """
         An error in the post-hook stops the chain and returns the error to the
         user.
         """
 
-    def test_endpointGETArgs(self):
+    def test_endpoint_GET_args(self):
         """
         An endpoint is matched when the GET arguments change.
         """
     
-    def test_endpointGlobbing(self):
+    def test_endpoint_globbing(self):
         """
         An endpoint is matched when there are '*' characters in the string
         """
--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     install_requires=[
         "Twisted == 14.0.0",
         "PyYAML == 3.10",
+        "treq == 0.2.1",
         ],
     # Some "trove classifiers" which are relevant.
     classifiers=[
