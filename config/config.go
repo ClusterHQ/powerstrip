42e551c33c53371fdd552a5f89be320086ed17ed
Matt Aitchison <Matt@lanciv.com>
2015-02-04 17:34:42 -0600

    Added config loading and request matching.

--- config/config.go
+++ config/config.go
@@ -3,25 +3,69 @@ package config
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"net/url"
+	"path"
 	"strings"
+
+	"gopkg.in/yaml.v1"
 )
 
+// Errors is used to return multiple errors from a validation func.
+type Errors []error
+
+func (e Errors) Error() string {
+	if len(e) == 1 {
+		return e[0].Error()
+	}
+
+	msg := "multiple errors:"
+	for _, err := range e {
+		msg += "\n" + err.Error()
+	}
+	return msg
+}
+
+// Config stores the entire config for powerstrip
 type Config struct {
 	Version   int
 	Endpoints map[string]Endpoint
 	Adapters  map[string]string
 }
 
+// Endpoint is an http endpoint on dockers api adapter.
 type Endpoint struct {
-	Method string
-	Path   string
-	Pre    []string
-	Post   []string
+	Method  string
+	Pattern string
+	Pre     []string
+	Post    []string
 }
 
-func (c *Config) Validate() []error {
-	errs := []error{}
+// NewConfig reads and parses a yaml config file
+func NewConfig(file string) (*Config, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	conf := &Config{}
+	err = yaml.Unmarshal(data, conf)
+	if err != nil {
+		return nil, err
+	}
+
+	errs := conf.Parse()
+	if errs != nil {
+		return nil, err
+	}
+
+	return conf, nil
+}
+
+// Parse validates and fills in values on the Config struct.
+func (c *Config) Parse() Errors {
+	var errs Errors
 	if len(c.Endpoints) == 0 {
 		errs = append(errs, errors.New("endpoints are required"))
 	}
@@ -47,7 +91,7 @@ func (c *Config) Validate() []error {
 
 		splitkey := strings.SplitN(key, " ", 2)
 		endpoint.Method = splitkey[0]
-		endpoint.Path = splitkey[1]
+		endpoint.Pattern = splitkey[1]
 
 		c.Endpoints[key] = endpoint
 
@@ -56,12 +100,30 @@ func (c *Config) Validate() []error {
 	return errs
 }
 
-// version: 1
-// endpoints:
-//   "POST /*/containers/create":
-//     pre: [flocker, weave]
-//   "POST /*/containers/*/start":
-//     post: [weave]
-// adapters:
-//   weave: http://weave/extension
-//   flocker: http://flocker/flocker-adapter
+// Match returns pre and post adapter urls for a request.
+func (c *Config) Match(req *http.Request) ([]string, []string) {
+	var preURLs, postURLs []string
+	for _, endpoint := range c.Endpoints {
+		match, _ := path.Match(endpoint.Pattern, req.URL.Path)
+
+		if match {
+			for _, name := range endpoint.Pre {
+				url, ok := c.Adapters[name]
+				if ok {
+					url = url + req.URL.Path
+					preURLs = append(preURLs, url)
+				}
+			}
+
+			for _, name := range endpoint.Post {
+				url, ok := c.Adapters[name]
+				if ok {
+					url = url + req.URL.Path
+					postURLs = append(postURLs, url)
+				}
+			}
+		}
+
+	}
+	return preURLs, postURLs
+}
--- config/config_test.go
+++ config/config_test.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"testing"
 
 	"gopkg.in/yaml.v2"
@@ -13,31 +15,57 @@ version: 1
 endpoints:
   "POST /*/containers/create":
     pre: [flocker, weave]
+    post: [flocker, weave]
   "POST /*/containers/*/start":
     post: [weave]
 adapters:
   weave: http://weave/extension
   flocker: http://flocker/flocker-adapter
 `
-var conf = `
+var invalidConfig = `
 version: 1
 endpoints:
   "POST /*/containers/create":
-    pre: [flocker, weave]
 adapters:
   weave:
   flocker: http://flocker/flocker-adapter
 `
 
-func TestConfig(test *testing.T) {
-	t := Config{}
-	err := yaml.Unmarshal([]byte(conf), &t)
+func TestNewConfig(t *testing.T) {
+	cfg, err := NewConfig("sample.yml")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if cfg == nil {
+		t.Error("config is nil")
+	}
+}
+
+func TestConfigParsing(t *testing.T) {
+	cfg := Config{}
+	err := yaml.Unmarshal([]byte(invalidConfig), &cfg)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
 
-	if err := t.Validate(); err != nil {
-		test.Log(t)
-		test.Error(err)
+	if err := cfg.Parse(); err == nil {
+		log.Fatalf("expected err got: %+v", cfg)
 	}
+}
+
+func TestConfigMatch(t *testing.T) {
+	cfg, err := NewConfig("sample.yml")
+	if err != nil {
+		t.Error(err)
+	}
+
+	req, _ := http.NewRequest("GET", "http://docker_host/v1.16/containers/json", nil)
+	pre, post := cfg.Match(req)
+	if pre == nil || post == nil {
+		t.Error("expected urls to not be nil")
+	}
+	fmt.Println(pre, post)
+
 }
