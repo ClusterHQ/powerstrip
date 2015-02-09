a3760ed515abf8c19290ac8df793b85201136b56
Luke Marsden <luke@clusterhq.com>
2015-02-09 11:52:57 -0800

    Four somewhat surprisingly passing tests

--- powerstrip/test/test_passthru.py
+++ powerstrip/test/test_passthru.py
@@ -11,11 +11,11 @@ $ TEST_PASSTHRU=1 trial powerstrip.test.test_passthru
 
 import os
 from twisted.internet import defer
-from twisted.internet.utils import getProcessOutput
 from twisted.trial.unittest import TestCase
 from ..testtools import GenerallyUsefulPowerstripTestMixin
 
-def CompareDockerAndPowerstrip(test_case, cmd):
+
+def CompareDockerAndPowerstrip(test_case, cmd, usePTY=False):
     """
     Compare the output of a real Docker server against a Powerstrip passthu.
 
@@ -32,19 +32,17 @@ def CompareDockerAndPowerstrip(test_case, cmd):
     DOCKER = b"unix:///var/run/docker.sock"
     POWERSTRIP = b"tcp://localhost:2375"
 
-    d = getProcessOutput(b"/bin/bash", ["-c", cmd], { b"DOCKER_HOST": DOCKER },
-        errortoo=True)
+    d = getProcessOutputPTY(b"/bin/bash", ["-c", cmd], { b"DOCKER_HOST": DOCKER },
+        errortoo=True, usePTY=usePTY)
 
     def got_result(docker_result):
         if not docker_result:
-            raise ValueError("Command did not produce any output went sent to "
+            raise ValueError("Command did not produce any output when sent to "
                     "Docker daemon.")
-        d = getProcessOutput(b"/bin/bash", ["-c", cmd], { b"DOCKER_HOST": POWERSTRIP },
-            errortoo=True)
+        d = getProcessOutputPTY(b"/bin/bash", ["-c", cmd], { b"DOCKER_HOST": POWERSTRIP },
+            errortoo=True, usePTY=usePTY)
 
         def compare_result(powerstrip_result, docker_result):
-            print "Got powerstrip result: %s" % (powerstrip_result,)
-            print "Got docker result: %s" % (docker_result,)
             test_case.assertEquals(docker_result, powerstrip_result)
             return powerstrip_result, docker_result
 
@@ -63,16 +61,6 @@ class BasicTests(TestCase, GenerallyUsefulPowerstripTestMixin):
     if "TEST_PASSTHRU" not in os.environ:
         skip = "Skipping passthru tests."
 
-    def setUp(self):
-        """
-        Actually run the current (local) version of powerstrip on
-        localhost:2375.
-        """
-        self._configure("endpoints: {}\nadapters: {}", dockerOnSocket=True,
-                realDockerSocket="/var/run/docker.sock",
-                powerstripPort=2375)
-        self.config.read_and_parse()
-
     def tearDown(self):
         shutdowns = [
             self.proxyServer.stopListening()]
@@ -84,18 +72,96 @@ class BasicTests(TestCase, GenerallyUsefulPowerstripTestMixin):
         """
         # XXX this will need to prime the Docker instance in most cases, e.g.
         # docker pull
+
+        # Actually run the current (local) version of powerstrip on
+        # localhost:2375.
+        self._configure("endpoints: {}\nadapters: {}", dockerOnSocket=True,
+                realDockerSocket="/var/run/docker.sock",
+                powerstripPort=2375)
+        self.config.read_and_parse()
         return CompareDockerAndPowerstrip(self,
             "docker run ubuntu echo hello")
+
+    def test_run_post_hook(self):
+        """
+        Test basic ``docker run`` functionality when there's a post-hook (the
+        post-hook should get skipped).
+        """
+        # Note that http://devnull/ should never be attempted because one
+        # should always skip a post-hook when Docker responds with a hijacked
+        # response type.
+        self._configure("""
+endpoints:
+  "POST /*/containers/*/attach":
+    post: [nothing]
+adapters:
+  nothing: http://devnull/
+""",
+                dockerOnSocket=True,
+                realDockerSocket="/var/run/docker.sock",
+                powerstripPort=2375)
+        self.config.read_and_parse()
+        return CompareDockerAndPowerstrip(self,
+            "docker run ubuntu echo hello")
+
+    def test_run_post_hook_tty(self):
+        """
+        Test basic ``docker run`` functionality with -ti when there's a
+        post-hook (the post-hook should get skipped).
+        """
+        self._configure("""
+endpoints:
+  "POST /*/containers/*/attach":
+    post: [nothing]
+adapters:
+  nothing: http://devnull/
+""",
+                dockerOnSocket=True,
+                realDockerSocket="/var/run/docker.sock",
+                powerstripPort=2375)
+        self.config.read_and_parse()
+        return CompareDockerAndPowerstrip(self,
+            "docker run -ti ubuntu echo hello", usePTY=True)
 
     def test_run_tty(self):
         """
         Test basic ``docker run`` functionality with -ti args. (terminal;
         interactive).
         """
-        # TODO: need to make twisted spawnProcess with a pty
+        self._configure("endpoints: {}\nadapters: {}", dockerOnSocket=True,
+                realDockerSocket="/var/run/docker.sock",
+                powerstripPort=2375)
+        self.config.read_and_parse()
         d = CompareDockerAndPowerstrip(self,
-            "docker run -ti ubuntu echo hello")
+            "docker run -ti ubuntu echo hello", usePTY=True)
         def assertions((docker, powerstrip)):
             self.assertNotIn("fatal", docker)
         d.addCallback(assertions)
         return d
+
+
+# XXX Ripped from twisted.internet.utils.getProcessOutput (to add PTY support
+# to getProcessOutput):
+from twisted.internet.utils import _BackRelay
+
+def getProcessOutputPTY(executable, args=(), env={}, path=None, reactor=None,
+                     errortoo=0, usePTY=False):
+    """
+    A version of getProcessOutput with a usePTY arg.
+    """
+    return _callProtocolWithDeferredPTY(lambda d:
+                                        _BackRelay(d, errortoo=errortoo),
+                                     executable, args, env, path,
+                                     reactor, usePTY=usePTY)
+
+def _callProtocolWithDeferredPTY(protocol, executable, args, env, path,
+        reactor=None, usePTY=False):
+    if reactor is None:
+        from twisted.internet import reactor
+    d = defer.Deferred()
+    p = protocol(d)
+    reactor.spawnProcess(p, executable, (executable,)+tuple(args), env, path,
+            usePTY=usePTY)
+    return d
+
+# End ripping.
