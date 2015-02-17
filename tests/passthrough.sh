1e46a4443eb2c1767e5977f55a02865c55bbf48c
Matt Aitchison <Matt@lanciv.com>
2015-02-17 17:22:11 -0600

    Most tests now passing.

--- tests/passthrough.sh
+++ tests/passthrough.sh
@@ -4,6 +4,19 @@ source "$(dirname $BASH_SOURCE)/util/testbed.sh"
 test-passthrough-prehook() {
 	in-testbed() {
 		start-inspect-adapter / pre-hook /tmp/output /dev/null
+		ip="$(docker inspect ${_inspects[0]} | jq -r ".[0].NetworkSettings.IPAddress")"
+		echo "${_inspects[@]}"
+		echo "$ip"
+
+		cat > /etc/powerstrip/adapters.yml <<EOF
+version: 1
+endpoints:
+  "POST /*/containers/*":
+    pre: [inspect]
+adapters:
+  inspect: http://$ip
+EOF
+
 		start-powerstrip
 		trap 'cleanup' EXIT
 		export DOCKER_HOST=tcp://localhost:2375
@@ -18,6 +31,20 @@ test-passthrough-prehook() {
 test-passthrough-posthook() {
 	in-testbed() {
 		start-inspect-adapter / post-hook /tmp/output /dev/null
+		ip="$(docker inspect ${_inspects[0]} | jq -r ".[0].NetworkSettings.IPAddress")"
+
+		echo "${_inspects[@]}"
+		echo "$ip"
+
+		cat > /etc/powerstrip/adapters.yml <<EOF
+version: 1
+endpoints:
+  "POST /*/containers/*":
+    post: [inspect]
+adapters:
+  inspect: http://$ip
+EOF
+
 		start-powerstrip
 		trap 'cleanup' EXIT
 		export DOCKER_HOST=tcp://localhost:2375
@@ -33,6 +60,20 @@ test-passthrough-pre-and-post() {
 	in-testbed() {
 		start-inspect-adapter / pre-hook /tmp/prehook /dev/null
 		start-inspect-adapter / post-hook /tmp/posthook /dev/null
+		ip="$(docker inspect ${_inspects[0]} | jq -r ".[0].NetworkSettings.IPAddress")"
+		echo "${_inspects[@]}"
+		echo "$ip"
+
+		cat > /etc/powerstrip/adapters.yml <<EOF
+version: 1
+endpoints:
+  "POST /*/containers/*":
+    pre: [inspect]
+    post: [inspect]
+adapters:
+  inspect: http://$ip
+EOF
+
 		start-powerstrip
 		trap 'cleanup' EXIT
 		export DOCKER_HOST=tcp://localhost:2375
@@ -42,5 +83,3 @@ test-passthrough-pre-and-post() {
 	}
 	use-testbed $FUNCNAME
 }
-
-
